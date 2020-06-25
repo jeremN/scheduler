c7f886b41185cbf435dd8fd01c5ce2458549dcaf
jeremN <jeremne@gmail.com>
2020-06-25 21:26:11 +0200

    feature/edit-planning: add calendar hours and day

--- src/components/organisms/Calendar/Calendar.js
+++ src/components/organisms/Calendar/Calendar.js
@@ -54,29 +54,40 @@ const Calendar = props => {
     let loopStart = moment(startHours, hoursFormat);
     let loopEnd = moment(startHours, hoursFormat);
     let roundedStart = moment(startHours, hoursFormat);
-    let roundedEnd = moment(startHours, hoursFormat);
 
     if (start.minutes != '00') {
       loopStart = 60 - start.minutes();
-      roundedStart = start.add(1, 'hour').startOf('hour');
+      roundedStart = moment(start, hoursFormat).add(1, 'hour').startOf('hour');
     }
 
     if (end.minutes != '00') {
       loopEnd = end.minutes();
-      roundedEnd = end.startOf('hour');
     }
-    hours.push(loopStart)
 
-    console.debug(start, end)
+    hours.push({
+      loopKey: moment(start, hoursFormat).format(hoursFormat),
+      content: '',
+      pxHeight: minutePxValue * loopStart
+    })
+
     for (let index = 0; index < hoursLen; index++) {
-      hours.push(moment(roundedStart, hoursFormat).add(index, 'hours').format(hoursFormat));
+      const hour = moment(roundedStart, hoursFormat).add(index, 'hours').format(hoursFormat);
+      hours.push({ 
+        loopKey: hour,
+        content: moment(hour, hoursFormat).format('HH[h]mm'),
+        pxHeight: minutePxValue * 60
+      });
     }
 
     if (hours[hours.length - 1] !== loopEnd) {
-      hours.push(loopEnd)
+      hours.push({
+        loopKey: moment(end).format(hoursFormat),
+        content: '',
+        pxHeight: minutePxValue * loopEnd
+      })
     }
-    console.debug(hours)
-    return { hoursTotal: hoursLen, pxValue: minutePxValue, hours: hours, hoursLimit: [start, end] };
+    console.debug('hours', hours)
+    return { hoursTotal: hoursLen, pxValue: minutePxValue, hours: hours };
   }
   
   const getWeekLength = () => {
@@ -92,38 +103,19 @@ const Calendar = props => {
   }
 
   const setHoursBlock = useMemo(() => {
-    const { hours, pxValue, hoursLimit } = getHoursLength();
-    const hourStyle = {
-      height: pxValue * 60
-    };
-    
+    const { hours } = getHoursLength();
+    console.debug('A', hours.length)
     return (
-      <Fragment>
         <ul className="calendar__hoursList">
-          { hours.map((hour, index) => {
-
-            if (!Number.isNaN(hour)) {
-              hourStyle.height = pxValue * hour;
-            }
-            console.debug(!Number.isNaN(hour) ? moment(hoursLimit[index === 0 ? 0 : 1], hoursFormat).format(hoursFormat) : moment(hour, hoursFormat).format(hoursFormat))
-            return (
-              <li 
-                key={ !Number.isNaN(hour) ? moment(hoursLimit[index === 0 ? 0 : 1], hoursFormat).format(hoursFormat) : moment(hour, hoursFormat).format(hoursFormat) } 
-                className="calendar__hour" 
-                style={ hourStyle }>{ !Number.isNaN(hour) ? '' : moment(hour, hoursFormat).format('HH[h]mm') }</li>
-            );
-          }) }
+        { hours.map(({ loopKey, pxHeight, content }) => <li key={ loopKey } style={ { height: `${pxHeight}px` } }>{ content }</li>) 
+        }
         </ul>
-      </Fragment>
     );
   }, [startHours, endHours]);
 
   const setWeekBlock = useMemo(() => {
     const weekArray = getWeekLength();
-    const { hours, pxValue, hoursTotal } = getHoursLength();
-    const hourStyle = {
-      height: pxValue * 60
-    };
+    const { hours } = getHoursLength();
 
     return (
       <Fragment>
@@ -131,8 +123,11 @@ const Calendar = props => {
           <div key={ moment(day, dayFormat).format(dayFormat) } className="calendar__dateBlock">
             <p className="calendar__date">{ moment(day, dayFormat).format('dddd') } <b>{ moment(day, dayFormat).format('DD') }</b></p>
             <ul className="calendar__dropzone">
-            { hours.map((hour) => (
-              <li key={ moment(hour, hoursFormat).format(hoursFormat) } className="calendar__hourBlock" style={ hourStyle }></li>
+            { hours.map(({ loopKey, pxHeight }) => (
+              <li 
+                key={ loopKey } 
+                className="calendar__hourBlock" 
+                style={ { height: `${pxHeight}px` } }></li>
               ))
             }
             </ul>
