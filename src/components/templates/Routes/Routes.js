e38991e1b8671ed76afd2d2fd11ad6d4dd806b82
jeremN <jeremne@gmail.com>
2020-08-22 22:36:12 +0200

    feature/auth: forget Routes js

---
+++ src/components/templates/Routes/Routes.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import routes from '../../../routes/routes';
+
+import { AuthContext } from '../../../App';
+
+const showOnAuth = ['isAuth', 'always'];
+const showNotAuth = ['notAuth', 'always'];
+
+const Routes = props => {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return (
+    <Switch>
+      { routes.map(({ path, components, exact, display }, index) => {
+        if ((isAuthenticated && showOnAuth.includes(display)) 
+          || (!isAuthenticated && showNotAuth.includes(display))) {
+          console.debug('route', path, isAuthenticated, display)
+          return ( 
+            <Route 
+              key={ index }
+              path={ path }
+              component={ components }
+              exact={ exact } />
+          );
+        }
+        return null
+      } )}
+      <Redirect to="/" />
+    </Switch>
+  )
+}
+
+export default Routes;
